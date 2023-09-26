5701b8ccf1ac9058e0b3d6163c0a523c3a655f22
Maddayum22 <madvanslooten@gmail.com>
2023-09-26 14:03:12 +0200

    added simple calculator project

---
+++ Simple Calculator/CalculatorEngine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate( string argOperation, double argFirstNum, double argSecondNum )
+        {
+            double result;
+
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNum + argSecondNum;
+                    break;
+                case "subtract":
+                case "-":
+                    result = argFirstNum - argSecondNum;
+                    break;
+                case "multiply":
+                case "*":
+                    result = argFirstNum * argSecondNum;
+                    break;
+                case "devide":
+                case "/":
+                    result = argFirstNum / argSecondNum;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified Operation is not recognized.");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Simple Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                Console.WriteLine("Enter first number:");
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Enter second number:");
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Enter operation type:");
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
