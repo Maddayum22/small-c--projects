c976ea435866be20b740771e95a500736cdd748c
Maddayum22 <madvanslooten@gmail.com>
2023-10-09 11:07:32 +0200

    File reader, mapper and displayer

---
+++ SudokuSolver/Workers/SudokuFileReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SudokuSolver.Workers
+{
+    public class SudokuFileReader
+    {
+        public int[,] ReadFile(string filename)
+        {
+            int[,] sudokuBoard = new int[9, 9];
+            try
+            {
+                var sudokuBoardLines = File.ReadAllLines(filename);
+
+                int row = 0;
+                foreach (var sudokuLine in sudokuBoardLines)
+                {
+                    string[] sudokuLineElements = sudokuLine.Split("|").Skip(1).Take(9).ToArray();
+
+                    int col = 0;
+                    foreach (var sudokuLineElement in sudokuLineElements)
+                    {
+                        sudokuBoard[row, col] = sudokuLineElement.Equals(" ") ? 0 : Convert.ToInt16(sudokuLineElement);
+                        col++;
+                    }
+                    row++;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong while reading the file: {ex.Message}");
+            }
+            return sudokuBoard;
+        }
+    }
+}
